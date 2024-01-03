c25f68f5d693bdbbfd8e7eb630d1e766130427d3
David House <davidahouse@gmail.com>
2024-01-02 20:46:02 -0500

    :wrench: updated options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,6 +1,14 @@
-
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
+
+vim.opt.smartindent = true
+vim.opt.wrap = false
+
+vim.cmd("set colorcolumn=80")
+vim.cmd("set scrolloff=8")
+vim.opt.guicursor = ""
+vim.opt.nu = true
+vim.opt.relativenumber = true
